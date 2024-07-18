e3849118e2281599a6d7077a2dafbae1e5a4440e
Kugapriya <159412262+Kugapriya@users.noreply.github.com>
2024-07-18 08:39:29 +0530

    End of section 6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, inject, input, Input, output, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
   private accountservice=inject(AccountService);
+  private toastr=inject(ToastrService)
   // usersFromHomeComponent=input.required<any>();
    @Input() usersFromHomeComponent:any;
   //  @Output() cancelRegister=new EventEmitter();
@@ -24,7 +26,8 @@ register()
     console.log(response);
     this.cancel();
   },
-  error:error=>console.log(error)
+  error:error=>this.toastr.error(error.error)
+
  })
 }
 
